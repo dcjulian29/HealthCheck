5630eee158dd5b8d5573980ded01fbbb10f8ea1f
Julian Easterling <julian@julianscorner.com>
2016-11-26 17:42:02 -0500

    Added an interface to handle the status of a plugin

---
+++ HealthCheck.Interfaces/PluginStatus.cs
@@ -0,0 +1,29 @@
+﻿namespace HealthCheck
+{
+    /// <summary>
+    /// Status of the plug-in.
+    /// </summary>
+    public enum PluginStatus
+    {
+        /// <summary>
+        /// The plug-in is initializing.
+        /// </summary>
+        Init,
+
+        /// <summary>
+        /// The plug-in has completed initializing but has not executed.
+        /// </summary>
+        Idle,
+
+        /// <summary>
+        /// The plug-in is executing but has not completed.
+        /// </summary>
+        Executing,
+
+        /// <summary>
+        /// The plug-in failed to complete the check due to an exception that could not be handled or
+        /// was unexpected.
+        /// </summary>
+        TaskExecutionFailure
+    }
+}
