1e7af1c0a11537282bbc4552f84987d2fab397f9
Julian Easterling <julian@julianscorner.com>
2016-11-26 17:40:35 -0500

    Added an enumeration for possible health check results.

---
+++ HealthCheck.Interfaces/CheckResult.cs
@@ -0,0 +1,26 @@
+﻿namespace HealthCheck
+{
+    /// <summary>
+    /// List of possible health check results.
+    /// </summary>
+    public enum CheckResult
+    {
+        /// <summary>
+        /// The health check has finished executing and returned a successful check. This is similar
+        /// to a green light on a stop light.
+        /// </summary>
+        Success,
+
+        /// <summary>
+        /// The health check has finished executing and returned a successful check with at least one
+        /// warning. This is similar to a yellow light on a stop light.
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// The health check has finished executing and return a failed check. This is similar to a
+        /// red light on a stop light.
+        /// </summary>
+        Error
+    }
+}
