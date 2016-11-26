d4290863e87c1d8f7040aba115f7b5a7dfffb83e
Julian Easterling <julian@julianscorner.com>
2016-11-26 17:41:30 -0500

    Added an interface to handle health checks that may need to send email notifications.

---
+++ HealthCheck.Interfaces/IMailDelieveryStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Net.Mail;
+
+namespace HealthCheck
+{
+    /// <summary>
+    /// The result of an execution of the health check that may need to send out email messages.
+    /// </summary>
+    public interface IMailDeliveryStatus
+    {
+        IEnumerable<MailMessage> Emails { get; set; }
+    }
+}
