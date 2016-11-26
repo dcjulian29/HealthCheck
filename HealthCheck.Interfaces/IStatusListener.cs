0ba8d860ae00eedfe47a35719fc53a4274ec8d8c
Julian Easterling <julian@julianscorner.com>
2016-11-26 18:07:15 -0500

    A status listener listens for and processes the result of an execution of a Health Check Plug-in.

---
+++ HealthCheck.Interfaces/IStatusListener.cs
@@ -0,0 +1,23 @@
+﻿namespace HealthCheck
+{
+    /// <summary>
+    /// A status listener listens for and processes the result of an execution of a Health Check Plug-in.
+    /// </summary>
+    public interface IStatusListener
+    {
+        /// <summary>
+        /// Gets or sets the threshold of this listener.
+        /// </summary>
+        CheckResult Threshold { get; set; }
+
+        /// <summary>
+        /// Perform any startup initialization.
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// Process a health check result.
+        /// </summary>
+        bool Process(IHealthStatus status);
+    }
+}
