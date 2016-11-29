eb97e1df86b3a4f32685d0f80734a04f464ade5a
Julian Easterling <julian@julianscorner.com>
2016-11-29 10:41:05 -0500

    A base implentation of the HealthCheck Service updated to use TopShelf

---
+++ HealthCheck/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using Common.Logging;
+using Topshelf;
+using Topshelf.Common.Logging;
+
+namespace HealthCheck
+{
+    internal class Program
+    {
+        private static ILog _log = LogManager.GetLogger<Program>();
+
+        private static void Main(string[] args)
+        {
+            var assembly = Assembly.GetEntryAssembly();
+
+            _log.Info(m => m(
+                "{0} {1} initialized on {2}",
+                assembly.GetName().Name,
+                assembly.GetName().Version.ToString(),
+                Environment.MachineName));
+
+            HostFactory.Run(x =>
+            {
+                x.Service<HealthService>(s =>
+                {
+                    s.ConstructUsing(n => new HealthService());
+                    s.WhenStarted(h => h.Start());
+                    s.WhenStopped(h => h.Stop());
+                });
+
+                x.UseCommonLogging();
+
+                x.RunAsNetworkService();
+
+                x.SetDescription("Serice Runtime for the Health Check Service");
+                x.SetDisplayName("Health Check Service");
+                x.SetServiceName("HealthCheck");
+            });
+        }
+    }
+}
---
+++ HealthCheck/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HealthCheck")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("HealthCheck")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4fc482b7-6d90-473c-81a6-47224d2932e9")]
