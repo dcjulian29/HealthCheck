5b19c9f3eda4db0073aa3ad0b4acfd18c1e1450f
Julian Easterling <julian@julianscorner.com>
2016-12-01 14:02:31 -0500

    Added a unit test project to solution and some unit test for HealthCheckJob and ComponentFactory.

---
+++ UnitTests/ComponentFactoryTests.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Xml.Linq;
+using Common.Logging;
+using HealthCheck.Framework;
+using Moq;
+using Xunit;
+
+namespace HealthCheck
+{
+    [SuppressMessage(
+         "StyleCop.CSharp.DocumentationRules",
+         "SA1600:ElementsMustBeDocumented",
+         Justification = "Test Suites do not need XML Documentation.")]
+    public class ComponentFactoryTests
+    {
+        public ComponentFactoryTests()
+        {
+            // Need to copy the Unit Test DLL so that MEF can find the exports below
+
+            if (!Directory.Exists("plugins"))
+            {
+                Directory.CreateDirectory("plugins");
+            }
+
+            File.Copy("UnitTests.dll", "plugins\\TestPlugin.dll", true);
+        }
+
+        [Fact]
+        public void GetPlugin_Should_LoadNamedInstance_When_SpecifyingByName()
+        {
+            // Arrange
+            var factory = new ComponentFactory();
+
+            // Act
+            var plugin1 = factory.GetPlugin("ContractName1");
+            var plugin2 = factory.GetPlugin("ContractName2");
+
+            // Assert
+            Assert.Equal(typeof(DummyPlugin1), plugin1.GetType());
+            Assert.Equal(typeof(DummyPlugin2), plugin2.GetType());
+        }
+
+        [Fact]
+        public void GetPlugin_Should_ReturnNull_When_PluginTypeDoesNotExist()
+        {
+            // Arrange
+            var factory = new ComponentFactory("plugins");
+
+            // Act
+            var plugin = factory.GetPlugin("NonExistentPlugin");
+
+            // Assert
+            Assert.Null(plugin);
+        }
+
+        [Fact]
+        public void GetPlugin_Should_ThrowInvalidArgumentException_When_TypeNameIsEmpty()
+        {
+            // Arrange
+            var factory = new ComponentFactory();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var plugin = factory.GetPlugin(String.Empty);
+            });
+        }
+
+        [PartCreationPolicy(CreationPolicy.NonShared)]
+        [Export("Listen1", typeof(IStatusListener))]
+        public class DummyListener1 : IStatusListener
+        {
+            private static ILog _log = LogManager.GetLogger<DummyListener1>();
+
+            public CheckResult Threshold { get; set; }
+
+            public void Initialize()
+            {
+            }
+
+            public bool Process(IHealthStatus status)
+            {
+                _log.Warn(status.Status);
+
+                return true;
+            }
+        }
+
+        [Export("Listen2", typeof(IStatusListener))]
+        public class DummyListener2 : IStatusListener
+        {
+            private static ILog _log = LogManager.GetLogger<DummyListener2>();
+
+            public CheckResult Threshold { get; set; }
+
+            public void Initialize()
+            {
+            }
+
+            public bool Process(IHealthStatus status)
+            {
+                _log.Warn(status.Status);
+
+                return true;
+            }
+        }
+
+        [PartCreationPolicy(CreationPolicy.NonShared)]
+        [Export("ContractName1", typeof(IHealthCheckPlugin))]
+        public class DummyPlugin1 : IHealthCheckPlugin
+        {
+            public string GroupName { get; set; }
+
+            public string Name { get; set; }
+            public PluginStatus PluginStatus { get; set; }
+
+            public IHealthStatus Execute()
+            {
+                return new Mock<IHealthStatus>().Object;
+            }
+
+            public void SetTaskConfiguration(XElement configurationElement)
+            {
+            }
+
+            public void Shutdown()
+            {
+            }
+
+            public void Startup()
+            {
+            }
+        }
+
+        [Export("ContractName2", typeof(IHealthCheckPlugin))]
+        public class DummyPlugin2 : IHealthCheckPlugin
+        {
+            public string GroupName { get; set; }
+
+            public string Name { get; set; }
+
+            public PluginStatus PluginStatus { get; set; }
+
+            public IHealthStatus Execute()
+            {
+                return new Mock<IHealthStatus>().Object;
+            }
+
+            public void SetTaskConfiguration(XElement configurationElement)
+            {
+            }
+
+            public void Shutdown()
+            {
+            }
+
+            public void Startup()
+            {
+            }
+        }
+
+        [Export("Namespace.ContractName2", typeof(IHealthCheckPlugin))]
+        public class DummyPlugin3 : IHealthCheckPlugin
+        {
+            public string GroupName { get; set; }
+
+            public string Name { get; set; }
+
+            public PluginStatus PluginStatus { get; set; }
+
+            public IHealthStatus Execute()
+            {
+                return new Mock<IHealthStatus>().Object;
+            }
+
+            public void SetTaskConfiguration(XElement configurationElement)
+            {
+            }
+
+            public void Shutdown()
+            {
+            }
+
+            public void Startup()
+            {
+            }
+        }
+
+        [Export("Namespace.ContractName3", typeof(IHealthCheckPlugin))]
+        public class DummyPlugin4 : IHealthCheckPlugin
+        {
+            public string GroupName { get; set; }
+
+            public string Name { get; set; }
+
+            public PluginStatus PluginStatus { get; set; }
+
+            public IHealthStatus Execute()
+            {
+                return new Mock<IHealthStatus>().Object;
+            }
+
+            public void SetTaskConfiguration(XElement configurationElement)
+            {
+            }
+
+            public void Shutdown()
+            {
+            }
+
+            public void Startup()
+            {
+            }
+        }
+
+        [Export("Namespace.ContractName3", typeof(IHealthCheckPlugin))]
+        public class DummyPlugin5 : IHealthCheckPlugin
+        {
+            public string GroupName { get; set; }
+
+            public string Name { get; set; }
+
+            public PluginStatus PluginStatus { get; set; }
+
+            public IHealthStatus Execute()
+            {
+                return new Mock<IHealthStatus>().Object;
+            }
+
+            public void SetTaskConfiguration(XElement configurationElement)
+            {
+            }
+
+            public void Shutdown()
+            {
+            }
+
+            public void Startup()
+            {
+            }
+        }
+    }
+}
---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d1c190bb-f2a0-47cb-b4cd-13f9c654d82f")]
