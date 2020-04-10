139f8c7147d10756a090d4c21ce61e914596bfec
Julian Easterling <julian@julianscorner.com>
2020-04-09 20:51:02 -0400

    Created a base implementation of IHealthStatus and UnitTests

--- HealthCheck.Interfaces/HealthStatus.cs
+++ HealthCheck.Interfaces/HealthStatus.cs
@@ -5,41 +5,49 @@ namespace HealthCheck
     /// <summary>
     ///   The result of an execution of the health check.
     /// </summary>
-    public interface IHealthStatus
+    public class HealthStatus : IHealthStatus
     {
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="HealthStatus" /> class.
+        /// </summary>
+        public HealthStatus()
+        {
+            Timestamp = DateTime.Now;
+        }
+
         /// <summary>
         ///   Gets or sets the details of the result of the health check
         /// </summary>
-        string Details { get; set; }
+        public string Details { get; set; }
 
         /// <summary>
         ///   Gets or sets the instance id of the component being health checked.
         /// </summary>
-        int InstanceId { get; set; }
+        public int InstanceId { get; set; }
 
         /// <summary>
         ///   Gets or sets the plugin that generated the status message
         /// </summary>
-        IHealthCheckPlugin Plugin { get; set; }
+        public IHealthCheckPlugin Plugin { get; set; }
 
         /// <summary>
         ///   Gets or sets the status of the health check.
         /// </summary>
-        CheckResult Status { get; set; }
+        public CheckResult Status { get; set; }
 
         /// <summary>
         ///   Gets or sets the status code of the result of the health check.
         /// </summary>
-        int StatusCode { get; set; }
+        public int StatusCode { get; set; }
 
         /// <summary>
         ///   Gets or sets the summary of the result of the health check.
         /// </summary>
-        string Summary { get; set; }
+        public string Summary { get; set; }
 
         /// <summary>
         ///   Gets or sets the date and time of the health check status.
         /// </summary>
-        DateTime Timestamp { get; set; }
+        public DateTime Timestamp { get; set; }
     }
 }
