0135705d272e1fb4684b2551cfe2683479b8e423
Julian Easterling <julian@julianscorner.com>
2020-04-12 19:06:01 -0400

    Make the individual status classes derive from base implementation of Health Status.

--- HealthCheck.Plugins/FreeMemoryStatus.cs
+++ HealthCheck.Plugins/FreeMemoryStatus.cs
@@ -1,17 +1,10 @@
-using System;
-
 namespace HealthCheck.Plugins
 {
     /// <summary>
     ///   Result from Free Memory plugin
     /// </summary>
-    public class FreeMemoryStatus : IHealthStatus
+    public class FreeMemoryStatus : HealthStatus
     {
-        /// <summary>
-        ///   Gets or sets the details of the result of the health check
-        /// </summary>
-        public string Details { get; set; }
-
         /// <summary>
         ///   Gets or sets the free space of memory.
         /// </summary>
@@ -23,38 +16,8 @@ namespace HealthCheck.Plugins
         public int FreeSpacePercent { get; set; }
 
         /// <summary>
-        ///   Gets or sets the instance id of the component being health checked.
-        /// </summary>
-        public int InstanceId { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the plugin that generated the status message
-        /// </summary>
-        public IHealthCheckPlugin Plugin { get; set; }
-
-        /// <summary>
         ///   Gets or sets the total size of memory.
         /// </summary>
         public long Size { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the status of the health check.
-        /// </summary>
-        public CheckResult Status { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the status code of the result of the health check.
-        /// </summary>
-        public int StatusCode { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the summary of the result of the health check.
-        /// </summary>
-        public string Summary { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the date and time of the health check status.
-        /// </summary>
-        public DateTime Timestamp { get; set; }
     }
 }
--- HealthCheck.Plugins/ProcessMemoryStatus.cs
+++ HealthCheck.Plugins/ProcessMemoryStatus.cs
@@ -1,22 +1,10 @@
-using System;
-
 namespace HealthCheck.Plugins
 {
     /// <summary>
     ///   Result from Free Memory plugin
     /// </summary>
-    public class ProcessMemoryStatus : IHealthStatus
+    public class ProcessMemoryStatus : HealthStatus
     {
-        /// <summary>
-        ///   Gets or sets the details of the result of the health check
-        /// </summary>
-        public string Details { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the instance id of the component being health checked.
-        /// </summary>
-        public int InstanceId { get; set; }
-
         /// <summary>
         ///   Gets or sets the amount of memory being used by process.
         /// </summary>
@@ -28,33 +16,8 @@ namespace HealthCheck.Plugins
         public long MemoryUsedMB => MemoryUsed / 1048576;
 
         /// <summary>
-        ///   Gets or sets the plugin that generated the status message
-        /// </summary>
-        public IHealthCheckPlugin Plugin { get; set; }
-
-        /// <summary>
         ///   Gets or sets the name of the process.
         /// </summary>
         public string ProcessName { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the status of the health check.
-        /// </summary>
-        public CheckResult Status { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the status code of the result of the health check.
-        /// </summary>
-        public int StatusCode { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the summary of the result of the health check.
-        /// </summary>
-        public string Summary { get; set; }
-
-        /// <summary>
-        ///   Gets or sets the date and time of the health check status.
-        /// </summary>
-        public DateTime Timestamp { get; set; }
     }
 }
